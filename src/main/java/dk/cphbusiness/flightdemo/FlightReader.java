ffa226b873600a5904491adb9effdf2b1a5a6d01
AndreasKuke <marslevandreas2@gmail.com>
2025-08-20 11:57:18 +0200

    Added new tests in FlightReaderTest.java Added new methods in FlightReader.java

--- src/main/java/dk/cphbusiness/flightdemo/FlightReader.java
+++ src/main/java/dk/cphbusiness/flightdemo/FlightReader.java
@@ -10,6 +10,7 @@ import java.nio.file.Paths;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Purpose:
@@ -22,7 +23,10 @@ public class FlightReader {
         try {
             List<FlightDTO> flightList = getFlightsFromFile("flights.json");
             List<FlightInfoDTO> flightInfoDTOList = getFlightInfoDetails(flightList);
-            flightInfoDTOList.forEach(System.out::println);
+//            flightInfoDTOList.forEach(System.out::println);
+
+            getSpecificAirlineFlightTotal(flightList, "Lufthansa");
+            getDepartureArrival(flightList,"Fukuoka","Haneda Airport");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -64,4 +68,58 @@ public class FlightReader {
         return flightInfoList;
     }
 
-}
+    public static List<FlightInfoDTO> getSpecificAirlineFlightTotal(List<FlightDTO> flightList, String airline) {
+         List<FlightInfoDTO> flightInfoDTOList =
+        flightList.stream()
+                .filter(flight -> airline.equalsIgnoreCase(flight.getAirline().getName()))
+                .map(flight -> {
+                LocalDateTime departure = flight.getDeparture().getScheduled();
+                LocalDateTime arrival = flight.getArrival().getScheduled();
+                Duration duration = Duration.between(departure, arrival);
+
+                FlightInfoDTO flightInfoDTO = FlightInfoDTO.builder()
+                        .name(flight.getFlight().getNumber())
+                        .iata(flight.getFlight().getIata())
+                        .airline(flight.getAirline().getName())
+                        .duration(duration)
+                        .departure(departure)
+                        .arrival(arrival)
+                        .origin(flight.getDeparture().getAirport())
+                        .destination(flight.getArrival().getAirport())
+                        .build();
+
+                    return flightInfoDTO;
+                }).toList();
+        System.out.println(flightInfoDTOList.size());
+        Duration totalDuration = flightInfoDTOList.stream().map(FlightInfoDTO::getDuration).reduce(Duration.ZERO,Duration::plus);
+        System.out.println(totalDuration.toHours() + totalDuration.toMinutes()%60);
+        return flightInfoDTOList;
+    }
+
+
+    public static Duration getAverageDurationAirline(List<FlightDTO> flightList, String airline) {
+        getSpecificAirlineFlightTotal(flightList, airline);
+
+        List<Duration> durations = flightList.stream().filter(flight -> airline.equalsIgnoreCase(flight.getAirline().getName()))
+                .map(flight -> Duration.between(flight.getDeparture().getScheduled(), flight.getArrival().getScheduled()))
+                .toList();
+
+        long totalSeconds = durations.stream()
+                .mapToLong(Duration::getSeconds).sum();
+
+        long averageSeconds = totalSeconds / durations.size();
+
+        return Duration.ofSeconds(averageSeconds);
+
+    }
+
+    public static List<FlightDTO> getDepartureArrival (List<FlightDTO> flightList ,String departure, String arrival) {
+        List<FlightDTO> flightsList = flightList.stream()
+                .filter(flight -> departure.equalsIgnoreCase(flight.getDeparture().getAirport()))
+                .filter(flight -> arrival.equalsIgnoreCase(flight.getArrival().getAirport())).collect(Collectors.toList());
+
+        System.out.println(flightsList);
+        return flightsList;
+
+    }
+    }
--- src/test/java/dk/cphbusiness/flightdemo/FlightReaderTest.java
+++ src/test/java/dk/cphbusiness/flightdemo/FlightReaderTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.time.Duration;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -44,8 +45,50 @@ class FlightReaderTest {
             List<FlightInfoDTO> flightInfoList = reader.getFlightInfoDetails(flights);
             assertEquals("Royal Jordanian", flightInfoList.get(0).getAirline());
 
+
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    @DisplayName("Test getting specific airline and seeing duration")
+    void getInfoLufthansa() {
+        try {
+            List<FlightDTO> flights = reader.getFlightsFromFile("flights.json");
+            List<FlightInfoDTO> flightInfoLufthansa = reader.getSpecificAirlineFlightTotal(flights, "Lufthansa");
+            Duration expectedDuration = Duration.ofHours(4);
+            Duration actualDuration = flightInfoLufthansa.get(0).getDuration();
+            assertEquals(expectedDuration, actualDuration);
+            System.out.println(flightInfoLufthansa.get(0).getAirline());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    @DisplayName("Test for average hours of flights for specific airline")
+    void getAverageHours() {
+        try {
+            List<FlightDTO> flights = reader.getFlightsFromFile("flights.json");
+            Duration avgDuration = reader.getAverageDurationAirline(flights, "Lufthansa");
+
+            assertEquals(2,avgDuration.toHours());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    @DisplayName("Test for list of airplanes between two airports")
+    void testGetFlightsBetweenTwoAirports() throws IOException {
+        List<FlightDTO> flights = reader.getFlightsFromFile("flights.json");
+        List<FlightDTO> result = FlightReader.getDepartureArrival(flights, "Fukuoka", "Haneda Airport");
+
+        assertFalse(result.isEmpty(), "Error no airports");
+        for (FlightDTO flight : result) {
+            assertEquals("Fukuoka", flight.getDeparture().getAirport());
+            assertEquals("Haneda Airport", flight.getArrival().getAirport());
         }
     }
 }
